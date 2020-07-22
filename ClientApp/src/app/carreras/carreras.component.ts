88442375856abdce3613ec47cb010df25c986586
EmmanuelF <emmanuelfernandez600@gmail.com>
2020-07-22 08:29:27 -0300

    Se corrigio el bug de la lista en la seccion de carreras

--- ClientApp/src/app/carreras/carreras.component.ts
+++ ClientApp/src/app/carreras/carreras.component.ts
@@ -5,6 +5,7 @@ import { IMateria } from "../materias/imateria";
 import { ICarrera } from "./icarrera";
 import { ICarreraMateria } from "./carreras-materias/icarrera-materia";
 import { ThrowStmt } from "@angular/compiler";
+import { FormGroup, FormBuilder } from "@angular/forms";
 
 @Component({
   selector: "app-carreras",
@@ -12,111 +13,136 @@ import { ThrowStmt } from "@angular/compiler";
   styleUrls: ["./carreras.component.css"],
 })
 export class CarrerasComponent implements OnInit {
+  formGroup: FormGroup;
   carreraSeleccionada: number;
   materiaSeleccionada: number;
+
+  ListaCarreras: ICarrera[];
+  ListaMaterias: IMateria[];
+  ListaMateriasCarrera: IMateria[] = [];
+  ListaMateriasDisponibles: IMateria[] = [];
+
+  llegoTope: boolean;
   sePuedeCargar: boolean;
-  llegoTope:boolean;
-  listaCarreras: ICarrera[];
-  listaMaterias: IMateria[];
-  materiasPorCarrera: IMateria[];
-  materiasDisponibles: IMateria[];
 
   constructor(
     private carrerasService: CarrerasService,
-    private materiasService: MateriasService
-  ) {}
-
-  ngOnInit() {
+    private materiasService: MateriasService,
+    private fb: FormBuilder
+  ) {
     this.cargarCarreras();
     this.cargarMaterias();
+  }
+
+  ngOnInit() {
+    this.formGroup = this.fb.group({
+      carreras: [{value: null, disabled: true}],
+      materiasDisponibles: [{value: null, disabled: true}],
+    });
+
     this.llegoTope = false;
     this.sePuedeCargar = true;
-    this.carreraSeleccionada = 0;
-    this.materiaSeleccionada = 0;
   }
 
   actualizarInfo() {
     //Vuelvo a actualizar el combobox
-
-    this.getMateriasCarrera(this.carreraSeleccionada);
-    this.materiaSeleccionada = 0;
-    this.verificarCargaMaterias();
+    this.listarMateriasCarrera();
+    // this.verificarCargaMaterias();
   }
 
   //Mostrar o no la dropdownlist de materias a asignar
   verificarCargaMaterias() {
-        // console.log("Tope antes?: ", this.llegoTope);
-        // console.log("Se puede cargar antes?", this.sePuedeCargar);
+    // console.log("Tope antes?: ", this.llegoTope);
+    // console.log("Se puede cargar antes?", this.sePuedeCargar);
 
-      if (this.materiasPorCarrera.length == this.listaMaterias.length-1  && !this.llegoTope) {
-        this.llegoTope = true;
-        this.sePuedeCargar = false;
-      }
+    if (
+      this.ListaMateriasCarrera.length == this.ListaMaterias.length - 1 &&
+      !this.llegoTope
+    ) {
+      this.llegoTope = true;
+      this.sePuedeCargar = false;
+    }
 
-      if (this.materiasPorCarrera.length == this.listaMaterias.length)
-        this.sePuedeCargar = true;
+    if (this.ListaMateriasCarrera.length == this.ListaMaterias.length)
+      this.sePuedeCargar = true;
 
-        // console.log("Tope despues?: ", this.llegoTope);
-        // console.log("Se puede cargar despues?", this.sePuedeCargar);
-    
+    // console.log("Tope despues?: ", this.llegoTope);
+    // console.log("Se puede cargar despues?", this.sePuedeCargar);
   }
+
   cargarCarreras() {
     this.carrerasService.getCarreras().subscribe(
-      (carrerasApi) => (this.listaCarreras = carrerasApi),
-      (error) => console.log(error)
-    );
-  }
-
-  cargarMaterias() {
-    this.materiasService.getMaterias().subscribe(
-      (materiasApi) => (this.listaMaterias = materiasApi),
-      (error) => console.log(error)
-    );
-  }
-
-  cargarInfoCarrera(event) {
-    this.carreraSeleccionada = parseInt(event.target.value);
-    this.getMateriasCarrera(this.carreraSeleccionada);
-  }
-
-  getMateriasCarrera(codCarrera: number) {
-    this.carrerasService.getMateriasCarrera(codCarrera).subscribe(
-      (apiMaterias) => {
-        this.materiasPorCarrera = apiMaterias;
-        this.getMateriasDisponibles(apiMaterias);
+      (carrerasApi) => {
+        // debugger;
+        this.ListaCarreras = carrerasApi;
+        this.formGroup.get("carreras").enable();
       },
       (error) => console.log(error)
-    );
+      );
+    }
+    
+  cargarMaterias() {
+    this.materiasService.getMaterias().subscribe(
+      (materiasApi) => (this.ListaMaterias = materiasApi),
+      (error) => console.log(error)
+      );
+    }
+    
+  listarMateriasCarrera() {
+    //Obtengo la carrera selecciona con el [formGroup]
+    this.carreraSeleccionada = this.formGroup.get("carreras").value;
+    this.formGroup.get('materiasDisponibles').reset();
+
+    
+    console.log("Cod.Carrera: ", this.carreraSeleccionada);
+    if (this.carreraSeleccionada) {
+      this.carrerasService
+      .getMateriasCarrera(this.carreraSeleccionada)
+      .subscribe(
+        (apiMaterias) => {
+          this.ListaMateriasCarrera = apiMaterias;
+
+          console.log("Materias Carrera:");
+          console.log(this.ListaMateriasCarrera);
+          this.getMateriasDisponibles(apiMaterias);
+          this.formGroup.get("materiasDisponibles").enable();
+          
+          },
+          (error) => console.log(error)
+        );
+    }
   }
 
   //Una lista de las materias que todavia se pueden anotar
   getMateriasDisponibles(materiasCarrera: IMateria[]) {
-    this.materiasDisponibles = this.listaMaterias.filter(
-      (m) => !this.estaAsignada(m, materiasCarrera)
+    let codMateriasCarrera: number[] = this.ListaMateriasCarrera.map(
+      (m) => m.codMateria
     );
+
+    console.log("Cod.Materias:");
+    console.log(codMateriasCarrera);
+
+    this.ListaMateriasDisponibles = this.ListaMaterias.filter(
+      (m) => !codMateriasCarrera.includes(m.codMateria)
+    );
+
+    console.log("Materias disponibles:");
+    console.log(this.ListaMateriasDisponibles);
   }
 
-  private estaAsignada(materia: IMateria, materias: IMateria[]): boolean {
-    for (let materiaCarrera of materias) {
-      if (materia.codMateria === materiaCarrera.codMateria) return true;
-    }
-    return false;
-  }
+  eliminarMateria(codMateria:number) {
+    debugger;
+    setTimeout((f) => this.actualizarInfo(), 1500);
 
-  eliminarMateria(codMateria: number) {
-    setTimeout((f) => this.actualizarInfo(), 1000);
-
-    //ASYNC REQUEST - llega luego
     this.carrerasService
-      .eliminarMateriaDeCarrera(this.carreraSeleccionada, codMateria)
+      .eliminarMateriaDeCarrera(
+        this.carreraSeleccionada,
+        codMateria
+      )
       .subscribe(
-        (materiaCarreraApi) => console.log(materiaCarreraApi),
+        (materiaCarreraApi) => console.log("Materia eliminada"),
         (error) => console.log(error)
       );
-  }
-
-  getMateriaSeleccionada(event) {
-    this.materiaSeleccionada = parseInt(event.target.value);
   }
 
   //Tengo que obtener el codigo de la materia de la select list de materias disponibles
@@ -126,7 +152,11 @@ export class CarrerasComponent implements OnInit {
       codMateria: this.materiaSeleccionada,
     };
 
-    setTimeout((f) => this.actualizarInfo(), 1000);
-    this.carrerasService.cargarMateriaEnCarrera(nuevaMateria).subscribe();
+    setTimeout((f) => this.actualizarInfo(), 1500);
+
+    this.carrerasService.cargarMateriaEnCarrera(nuevaMateria).subscribe(
+      () => console.log("Materia agregada"),
+      (error) => console.log(error)
+    );
   }
 }
